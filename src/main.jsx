d30f8ad5b45ef3805a0b423ca7106c6221ee3be7
abdelrahman027 <116889922+abdelrahman027@users.noreply.github.com>
2025-05-19 19:21:44 +0300

    fixing pixel facebook

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <I18nextProvider i18n={i18n}>
+      <Analytics />
       <App />
     </I18nextProvider>
   </StrictMode>,
