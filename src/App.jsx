014d1a6ce797e2dfb7672d437ba5c55bc4c279f7
abdelrahman027 <116889922+abdelrahman027@users.noreply.github.com>
2025-05-19 19:24:15 +0300

    fixing vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Destinations from './components/Destinations'
 import About from './components/About'
 import StartForm from './components/StartForm'
 import Footer from './components/Footer'
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
     <div className='overflow-x-hidden flex flex-col items-center justify-center'>
+      <Analytics />
       <Hero />
       <Destinations />
       <About />
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
-import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <I18nextProvider i18n={i18n}>
-      <Analytics />
       <App />
     </I18nextProvider>
   </StrictMode>,
